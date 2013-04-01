41edea9a334252db8cddb5239f6fd006aaaf7c25
imrefazekas <imre.fazekas@gmail.com>
2013-04-01 09:56:26 +0200

    code refinements

--- test/restBuilder.js
+++ test/restBuilder.js
@@ -11,27 +11,27 @@ function buildUpRestAPI( rest, _ ){
 	});
 	rest.post('/store/?id', function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback(null, 'ok');
+		return callback(null, 'ok');
 	});
 	rest.get('/inquire/*book', function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback(null, 'ok');
+		return callback(null, 'ok');
 	});
 	rest.get( '/set/?rid/?facet', function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback(null, 'ok');
+		return callback(null, 'ok');
 	});
 	rest.post( { path: '/make', version: '>=1.0.0' }, function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback(null, 'ok');
+		return callback(null, 'ok');
 	});
 	rest.post( [ '/act', '/do' ], function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback( null, 'ok' );
+		return callback( null, 'ok' );
 	});
 	rest.post( [ { path: '/shake', version: '>=2.0.0' }, { path: '/twist', version: '>=2.1.1' } ], function( request, content, callback ){
 		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
-		callback(null, 'ok');
+		return callback(null, 'ok');
 	}, {'title': 'Alice in Wonderland'} );
 }
 
