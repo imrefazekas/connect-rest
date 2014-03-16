4b6e720ba12ee08025baecea6e5db4f3e0d0b815
Imre Fazekas <imre.fazekas@icloud.com>
2014-03-16 09:53:14 +0100

    request object refinements

--- test/restBuilder.js
+++ test/restBuilder.js
@@ -2,52 +2,52 @@ function buildUpRestAPI( rest ){
 	//rest.context( '/api' );
 
 	rest.head('/peek', function( request ){
-		console.log( 'Received:' + JSON.stringify( request ) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) );
 		return 'ok';
 	});
 	rest.get('/empty', function( request ){
-		console.log( 'Received:' + JSON.stringify( request ) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) );
 		return '';
 	});
 	rest.get('/books/:title/:chapter', function( request ){
-		console.log( 'Received:' + JSON.stringify( request ) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) );
 		return request.parameters;
 	});
 	rest.post('/store/?id', function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, { params: request.parameters, content: content} );
 	});
 	rest.get('/inquire/*book', function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, 'ok');
 	});
 	rest.get( '/set/?rid/?facet', function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, request.params, { minify: true } );
 	});
 	rest.get( { path: '/eset/?rid/?facet', version: '*', protector: function(){ return true; } }, function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, request.params, { minify: true } );
 	});
 	rest.get( '/minify', function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, '{ "key"     :    "value" }', { minify: true } );
 	});
 	rest.post( { path: '/make', version: '>=1.0.0' }, function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, 'ok');
 	});
 	rest.post( [ '/act', '/do' ], function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback( null, 'ok' );
 	});
 	rest.post( [ { path: '/shake', version: '>=2.0.0' }, { path: '/twist', version: '>=2.1.1' } ], function( request, content, callback ){
-		console.log( 'Received:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, 'ok');
 	}, {'title': 'Alice in Wonderland'} );
 
 	rest.get( '/data/items', function( request, content, callback ){
-		console.log( 'Received::' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Received::' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, request.parameters, {statusCode:201} );
 	}, { contentType:'application/json', validator: function(req, res){ return true; } } );
 
@@ -62,7 +62,7 @@ function buildUpRestAPI( rest ){
 	}, { contentType:'application/json' } );
 
 	rest.post( '/upload', function( request, content, callback ){
-		console.log( 'Upload called:' + JSON.stringify( request ) + ' ' + JSON.stringify(content) );
+		console.log( 'Upload called:' + JSON.stringify( { headers: request.headers, parameters: request.parameters, session: request.session } ) + ' ' + JSON.stringify(content) );
 		return callback(null, 'ok');
 	} );
 
